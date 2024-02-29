ebd7d714e130800f32dae461232301c85712498b
Joakim Lindh <joakim.lindh@epndata.com>
2024-02-29 11:01:46 +0100

    lektion 2

--- Lektion-2/2-contact-list/public/index.js
+++ Lektion-2/2-contact-list/public/index.js
@@ -1,6 +1,68 @@
+const output = document.querySelector('#output')
+const contacts = []
+
 const getContacts = async () => {
-  const res = await fetch('/api/contacts')
-  const data = await res.json()
-  console.log({res, data})
+  try {
+    const res = await fetch('/api/contacts')
+
+    if(res.status !== 200) {
+      throw new Error('something went wrong')
+    }
+
+    const data = await res.json()
+    data.forEach(contact => contacts.push(contact))
+
+    listContacts()
+  } catch (error) {
+    console.log(error.message)
+  }
 }
 getContacts()
+
+const listContacts = () => {
+  output.innerHTML = ''
+  contacts.forEach(contact => {
+    output.innerHTML += `
+    <div class="contact">
+      <p class="name">${contact.firstName} ${contact.lastName}</p>
+      <p class="phone">${contact.phoneNumber}</p>
+      <button class="del-btn">X</button>
+    </div>
+    `
+  })
+}
+
+
+const addContact = async (contact) => {
+  const res = await fetch('/api/contacts', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(contact)
+  })
+  
+  const data = await res.json()
+
+  if(res.status !== 201) return
+
+  contacts.push(data)
+}
+
+document.querySelector('#addContactForm').addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const firstNameValue = document.querySelector('#firstName').value.trim()
+  const lastNameValue = document.querySelector('#lastName').value.trim()
+  const phoneValue = document.querySelector('#phone').value.trim()
+
+  if(firstNameValue == '' || lastNameValue == '' || phoneValue == '') return
+
+  await addContact({
+    firstName: firstNameValue,
+    lastName: lastNameValue,
+    phoneNumber: phoneValue
+  })
+
+  listContacts()
+})
--- Lektion-2/2-contact-list/server.js
+++ Lektion-2/2-contact-list/server.js
@@ -3,7 +3,11 @@ const path = require('path')
 const fs = require('fs')
 
 const app = express()
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
+
+const DB_CONNECTION = path.join(__dirname, 'contacts.json')
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('server running on: http://localhost:' + PORT))
@@ -11,6 +15,31 @@ app.listen(PORT, () => console.log('server running on: http://localhost:' + PORT
 
 app.get('/api/contacts', (req, res) => {
 
-  const contacts = fs.readFileSync(path.join(__dirname, 'contacts.json'), 'utf8')
+  const contacts = fs.readFileSync(DB_CONNECTION, 'utf8')
   res.status(200).send(contacts)
+})
+
+
+
+
+
+app.post('/api/contacts', (req, res) => {
+  const { firstName, lastName, phoneNumber } = req.body
+  
+  if(!firstName || !lastName || !phoneNumber) {
+    res.status(400).json({ message: 'please enter all the fields' })
+  }
+
+  const contacts = JSON.parse(fs.readFileSync(DB_CONNECTION, 'utf8'))
+  const newContact = {
+    id: crypto.randomUUID(),
+    firstName, 
+    lastName,
+    phoneNumber
+  }
+
+  contacts.push(newContact)
+
+  fs.writeFileSync(DB_CONNECTION, JSON.stringify(contacts, null, 2))
+  res.status(201).json(newContact)
 })
