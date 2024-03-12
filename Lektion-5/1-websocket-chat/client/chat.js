74aaef45b81d38c4580a1669190a97c3f7e5a2b1
Joakim Lindh <joakim.wahlstrom@epndata.com>
2024-03-12 11:04:14 +0100

    lektion 5

--- Lektion-5/1-websocket-chat/client/chat.js
+++ Lektion-5/1-websocket-chat/client/chat.js
@@ -1,3 +1,49 @@
-
+const socket = io();
 
 const userName = new URLSearchParams(window.location.search).get('username')
+document.querySelector('#me').innerText = userName;
+
+const messages = document.querySelector('.messages');
+
+
+
+
+// När vi som klient har kopplat upp oss
+socket.on('connect', () => {
+  // Skapar eventet 'user' och skickar med användarnamnet
+  socket.emit('user', userName)
+})
+
+// när en ny användare har anslutit
+socket.on('new_user_connection', (feedbackString) => {
+  // messages.innerHTML += `<p class="inline-feedback">${feedbackString}</p>`
+  messages.appendChild(createElement('p', 'inline-feedback', feedbackString))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Helpers
+// Bygg ihop ett element
+const createElement = (type, className, text) => {
+  const element = document.createElement(type)
+  element.className = className ? className : ''
+  element.innerText = text ? text : ''
+
+  return element
+}
---
+++ Lektion-5/1-websocket-chat/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const path = require('path')
+
+const app = express()
+const httpServer = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, 'client')))
+
+const PORT = process.env.PORT || 9999;
+
+httpServer.listen(PORT, () => console.log('Server running on http://localhost:' + PORT ))
+
+const io = new Server(httpServer)
+
+
+// socket.emit - skickar eventet till den specifika socket
+// io.emit - Skickar event till ALLA sockets som är anslutna
+// socket.broadcast.emit - Skcka event till alla andra sockets som är anslutna 
+
+io.on('connection', socket => {
+
+  socket.on('user', (userName) => {
+    socket.broadcast.emit('new_user_connection', `${userName} has entered the chat`)
+  })
+
+})
