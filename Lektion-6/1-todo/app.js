4a82d9d3a1316ebcdb015411bca936865753b70a
Joakim Lindh <joakim.lindh@epndata.com>
2024-03-14 10:52:13 +0100

    lektion 6

--- Lektion-6/1-todo/app.js
+++ Lektion-6/1-todo/app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dbConnect from './server.js'
 import todoRoutes from './routes/todoRoutes.js'
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+dbConnect()
 const app = express()
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('Server running on port: ' + PORT))
-dbConnect()
 
 
 app.use(express.json())
--- Lektion-6/1-todo/server.js
+++ Lektion-6/1-todo/server.js
@@ -6,7 +6,7 @@ const mongoUri = process.env.MONGO_URI
 
 const dbConnect = async () => {
   try {
-  
+    if(!mongoUri) throw new Error('Please define the MONGO_URI variable inside the .env file')
     const db = await mongoose.connect(mongoUri)
     console.log('DB connected');
   } catch (err) {
